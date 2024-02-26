be0ead46d0acc6f4867d9d2dfb4265579517891a
fabiobatoni <fabio_batoni@hotmail.com>
2024-02-25 21:26:56 -0300

    component: App: Create Global Style
    feat: Add global style in App.ts
    
    Signed-off-by: fabiobatoni <fabio_batoni@hotmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
+import { ThemeProvider } from "styled-components"
+import { defaultTheme } from "./styles/themes/default"
+import { GlobalStyle } from "./styles/themes/global"
+
 export function App() {
   return (
-    <>
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
      <h1>Hello World !</h1>
-    </>
+    </ThemeProvider>
   )
 }
 
