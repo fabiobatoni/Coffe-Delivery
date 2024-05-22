e4f738e1776976c9575f9c7ef181d32daf4d0a62
fabiobatoni <fabio_batoni@hotmail.com>
2024-05-22 09:22:30 -0300

    component: Hero: create new component Hero.tsx
    Separate information from the main component in Hero component
    
    Signed-off-by: fabiobatoni <fabio_batoni@hotmail.com>

---
+++ src/pages/Home/components/Hero.tsx
@@ -0,0 +1,67 @@
+import { Coffee, Package, ShoppingCartSimple, Timer } from 'phosphor-react';
+import logoHome from '../../../assets/HomeLogo.svg';
+import { HomeContainer, InfoContainer, SubTitlesContainer } from './styles';
+import { useTheme } from 'styled-components';
+
+export function Hero () {
+
+  const theme = useTheme()
+
+  return (
+    <div>
+      <HomeContainer>
+      <InfoContainer>
+        <h1>
+        Encontre o café perfeito para qualquer hora do dia
+        </h1>
+        <p>Com o Coffee Delivery você recebe seu café onde estiver, a qualquer hora</p>
+        
+        <SubTitlesContainer>
+          <div>
+            <ShoppingCartSimple 
+              size={32} 
+              weight="fill"
+              color={theme.background}
+              style={{ backgroundColor: theme['yellow-dark'] }}
+            />
+            <span>Compra simples e segura</span>
+          </div>
+         
+          <div>
+            <Timer 
+              size={32}
+              weight="fill"
+              color={theme.background}
+              style={{ backgroundColor: theme['yellow'] }} 
+            />
+            <span>Entrega rápida e rastreada</span>
+          </div>
+          
+          <div>
+            <Package 
+              size={32} 
+              weight="fill"
+              color={theme.background}
+              style={{ backgroundColor: theme['base-text'] }}
+            />
+            <span>Embalagem mantém o café intacto</span>
+          </div>
+
+          <div>
+            <Coffee 
+              size={32} 
+              weight="fill"
+              color={theme.background}
+              style={{ backgroundColor: theme['purple'] }}
+            />
+            <span>O café chega fresquinho até você</span>
+          </div>
+        </SubTitlesContainer>
+
+      </InfoContainer>
+
+      <img src={logoHome} alt=""></img>
+    </HomeContainer>
+    </div>
+  )
+}
