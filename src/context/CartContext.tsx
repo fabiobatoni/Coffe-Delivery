a7d29d8020d1c0fc2022c933c279c12567af99c1
fabiobatoni <fabio_batoni@hotmail.com>
2024-05-27 11:34:39 -0300

    component: context: CartContext: create reducer
    Add useReducer within context
    
    Signed-off-by: fabiobatoni <fabio_batoni@hotmail.com>

--- src/context/CartContext.tsx
+++ src/context/CartContext.tsx
@@ -1,4 +1,4 @@
-import { ReactNode, createContext, useState } from "react";
+import { ReactNode, createContext, useReducer, useState } from "react";
 
 export interface CoffeeDetails {
   id: number
@@ -25,35 +25,100 @@ interface CartContextProviderProps {
   children: ReactNode
 }
 
+enum ActionTypes {
+  ADD_COFFEE = 'ADD_COFFEE',
+  REDUCE_COFFEE = 'REDUCE_COFFEE',
+  REMOVE_COFFEE = 'REMOVE_COFFEE'
+}
+
+interface PayloadTypes {
+  id?: number
+  coffeeDetails?: CoffeeDetails
+}
+
+interface Action {
+  type: ActionTypes,
+  payload: PayloadTypes
+}
+
 export function CartContextProvider({ children } : CartContextProviderProps) {
 
-  const [coffeesOnCart, setCoffesOnCart] = useState<CoffeeDetails[]>([]) 
+  const [ coffeesOnCart, dispatch ] = useReducer((state: CoffeeDetails[], action: Action ) => {
+    const { payload } = action
+    switch(action.type) {
+      case ActionTypes.ADD_COFFEE: 
+        const { coffeeDetails } = payload
+
+        if(!coffeeDetails) {
+          return state
+        }
+
+        const currentCoffeIndex = state.findIndex((coffee) => coffee.id === coffeeDetails.id)
+
+        if(currentCoffeIndex >= 0) {
+          return state.map((coffee, index) => index === currentCoffeIndex ? {...coffeeDetails, quantity: coffee.quantity + 1 } : coffee)
+        } else {
+          return [...state, {...coffeeDetails, quantity: 1}]
+        }
+    
+      case ActionTypes.REDUCE_COFFEE: {
+        const { id } = payload
+
+        if(!id) {
+          return state
+        }
+
+        const currentCoffeIndex = state.findIndex((coffee) => coffee.id === id)
+
+        if(currentCoffeIndex >= 0) {
+          return state.map((coffee, index) => index === currentCoffeIndex ? {...coffee, quantity: coffee.quantity - 1 } : coffee).filter((coffee) => coffee.quantity > 0)
+        }
+
+        return state
+      }
+
+      case ActionTypes.REMOVE_COFFEE: {
+
+        const { id } = payload
+
+        if(!id) {
+          return state
+        }
+
+        const currentCoffeIndex = state.findIndex((coffee) => coffee.id === id)
+
+        if(currentCoffeIndex >= 0) {
+          return state.filter((_, index) => index !== currentCoffeIndex)
+        }
+
+        return state
+      }
+
+
+      default:
+        return state
+    }
+  }, [])
 
   function addCoffeeToCart(coffeeDetails: CoffeeDetails) {
-    const currentCoffeIndex = coffeesOnCart.findIndex((coffee) => coffee.id === coffeeDetails.id)
-
-    if(currentCoffeIndex >= 0) {
-      setCoffesOnCart((state) => state.map((coffee, index) => index === currentCoffeIndex ? {...coffeeDetails, quantity: coffee.quantity + 1 } : coffee))
-    } else {
-      setCoffesOnCart(state => [...state, {...coffeeDetails, quantity: 1}])
-    }
-
+    return dispatch({
+      type: ActionTypes.ADD_COFFEE,
+      payload: { coffeeDetails }
+    })
   }
 
   function reduceCoffeFromCart(id: number) {
-    const currentCoffeIndex = coffeesOnCart.findIndex((coffee) => coffee.id === id)
-
-    if(currentCoffeIndex >= 0) {
-      setCoffesOnCart((state) => state.map((coffee, index) => index === currentCoffeIndex ? {...coffee, quantity: coffee.quantity - 1 } : coffee).filter((coffee) => coffee.quantity > 0))
-    }
+    return dispatch({
+      type: ActionTypes.REDUCE_COFFEE,
+      payload: { id }
+    })
   }
 
   function removeCoffeFromCart(id: number) {
-    const currentCoffeIndex = coffeesOnCart.findIndex((coffee) => coffee.id === id)
-
-    if(currentCoffeIndex >= 0) {
-      setCoffesOnCart((state) => state.filter((_, index) => index !== currentCoffeIndex))
-    }
+    return dispatch({
+      type: ActionTypes.REMOVE_COFFEE,
+      payload: { id }
+    })
   }
 
 
